cccd58e56aad8799e4ee85561589514715a486a0
tl1l1l1s <shj721553@gmail.com>
2024-07-23 22:51:31 +0900

    feat: adapter view

---
+++ software/AdapterViewTest/app/src/main/java/com/example/week10/adapterviewtest/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.week10.adapterviewtest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.week10.adapterviewtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
